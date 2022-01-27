06dca174ee1a65525eb23393e7ac10ce9bc9d23d
chuongvu2001 <chuongvu2806@gmail.com>
2022-01-27 22:50:43 +0700

    use state object

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import { useState } from 'react';
 
-const orders = [100,200,300];
-
 function App() {
-  const [counter,setCounter] = useState(()=>{
-    const total = orders.reduce((total,cur)=>total + cur
-    
-    );
-    return total;
+  const [info, setInfo] = useState({
+    name: 'CHUN',
+    age: 20,
+    address: 'Ha Noi'
   });
 
-  const handleIncrease = () => {
-    setCounter(counter + 1);
+  const handleUpdate = () => {
+    setInfo(prev => (
+      {
+        ...info,
+        bio: "Đẹp zai"
+      }
+    ));
   }
 
   return (
     <div className='App'>
       <h1>
-          {counter}
+        {JSON.stringify(info)}
       </h1>
-      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleUpdate}>Update</button>
     </div>
   );
 }
