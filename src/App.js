d69c04bad9e4c2641ee0069ebae8f1be31cfe586
chuongvu2001 <chuongvu2806@gmail.com>
2022-01-27 22:39:18 +0700

    state callback

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 
+const orders = [100,200,300];
+
 function App() {
-  const [counter,setCounter] = useState(1);
+  const [counter,setCounter] = useState(()=>{
+    const total = orders.reduce((total,cur)=>total + cur
+    
+    );
+    return total;
+  });
 
   const handleIncrease = () => {
     setCounter(counter + 1);
   }
+
   return (
     <div className='App'>
       <h1>
